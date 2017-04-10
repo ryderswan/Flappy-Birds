8ab749d03b577382b34e90be6741df2225f51735
Ryder Swan <oars@bartswan.com>
2017-04-10 10:25:45 -0500

    Changed fade out time

--- Flappy Birds/GameScene.swift
+++ Flappy Birds/GameScene.swift
@@ -24,15 +24,16 @@ class GameScene: SKScene {
         }
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
+        let w = (self.size.width + self.size.height) * 0.03
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
         
         if let spinnyNode = self.spinnyNode {
             spinnyNode.lineWidth = 2.5
             
             spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 1.0),
                                               SKAction.removeFromParent()]))
         }
     }
